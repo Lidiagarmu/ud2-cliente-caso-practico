59352da37b1550908fad001a312735263b6bbbc8
Lidia Garcia Muñoz <lidiagarmu@gmail.com>
2024-12-11 17:15:39 +0100

    caso practico ud2 terminado

--- restaurante app/src/scripts/script.js
+++ restaurante app/src/scripts/script.js
@@ -1,7 +1,5 @@
 
-//funcion que se va a lanzar cuando hagamos click en Reservar
-
-
+//funcion que se va a lanzar cuando hagamos click en Enviar menú
 
 //Seleccionamos el boton Enviar
 const buttonEnviar = document.querySelector("#enviar");
@@ -13,7 +11,7 @@ const handleClick = (event) => {
     //1 - evita la recarga o render de la pagina 
     event.preventDefault();
    
-    //2 - seleccionamos selects para ver su valor
+    //2 - seleccionamos los selects para ver su valor
     const primerPlatoSeleccionado = document.querySelector("#primer-plato").value;
     const segundoPlatoSeleccionado = document.querySelector("#segundo-plato").value;
     const postreSeleccionado = document.querySelector("#postre").value;
@@ -26,15 +24,14 @@ const handleClick = (event) => {
     }
 
 
-
     // 4.1 - seleccionamos el main entero y lo vaciamos
     const main = document.querySelector("main");
     main.innerHTML= "";
 
-    // 4.2 - creamos template dentro del script y se lo metemos al main para que muestre los datos de la reserva que introdujo el usuario
+    // 4.2 - creamos el contenido que queremos mostrar despueés de la selección de menú
     main.innerHTML= `  
         <div>
-        
+               <img src="/restaurante app/resources/images/logo.png" alt="Logo de Lidia's Restaurant" id="logo">
                 <div id="info">
                     <h2>Menú Seleccionado</h2>
                     <p>Gracias por elegir tu menú. Aquí están tus opciones:</p>
@@ -55,74 +52,76 @@ const handleClick = (event) => {
         // 5 - seleccionamos boton Volver. Y cuando hagamos clic sobre el:
 
         const volverButton = document.querySelector ("#back");
-        volverButton.addEventListener("click", () => {
-            
 
-           
-            
-             // 5.1 cambiamos el main de nuevo con un nuevo template dentro del script
+        volverButton.addEventListener("click", () => {
+            main.innerHTML = ""; // Limpiar el contenido de main
+        
+             // 5.2 volvemos a insertar el formulario original
              main.innerHTML = `
-             <main>
-                    <h1>ELIJA SU MENÚ</h1>
-                        <form id="menu-form">
-                        
-                        <div>
-                            <section>
-                                <h2>Primer Plato</h2>
-                                <select id="primer-plato" required>
-                                <option value="" disabled selected>Seleccione un primer plato</option>
-                                <option value="Ensalada Mixta">Ensalada Mixta</option>
-                                <option value="Sopa de Verduras">Sopa de Verduras</option>
-                                <option value="Crema de Calabaza">Crema de Calabaza</option>
-                                </select>
-                            </section>
+                <img src="/restaurante app/resources/images/logo.png" alt="Logo de Lidia's Restaurant" id="logo">
+                <h1>ELIJA SU MENÚ</h1>
+                
+                <form id="menu-form">
+            
+                    <div>
+                        <section>
+                            <h2>Primer Plato</h2>
+                            <select id="primer-plato" required>
+                            <option value="" disabled selected>Seleccione un primer plato</option>
+                            <option value="Ensalada Mixta">Ensalada Mixta</option>
+                            <option value="Sopa de Verduras">Sopa de Verduras</option>
+                            <option value="Crema de Calabaza">Crema de Calabaza</option>
+                            </select>
+                        </section>
+                    </div>
+
+                    <div>
+                        <section>
+                            <h2>Segundo Plato</h2>
+                            <select id="segundo-plato" required>
+                            <option value="" disabled selected>Seleccione un segundo plato</option>
+                            <option value="Pollo Asado">Pollo Asado</option>
+                            <option value="Merluza a la Plancha">Merluza a la Plancha</option>
+                            <option value="Escalopines de ternera">Escalopines de ternera</option>
+                            </select>
+                        </section>
                         </div>
 
                         <div>
-                            <section>
-                                <h2>Segundo Plato</h2>
-                                <select id="segundo-plato" required>
-                                <option value="" disabled selected>Seleccione un segundo plato</option>
-                                <option value="Pollo Asado">Pollo Asado</option>
-                                <option value="Merluza a la Plancha">Merluza a la Plancha</option>
-                                <option value="Lasagna Vegetariana">Escalopines de ternera</option>
-                                </select>
-                            </section>
-                            </div>
+                        <section>
+                            <h2>Postre</h2>
+                            <select id="postre" required>
+                            <option value="" disabled selected>Seleccione un postre</option>
+                            <option value="Tarta de Queso">Tarta de Queso</option>
+                            <option value="Flan de Huevo">Flan de Huevo</option>
+                            <option value="Fruta de Temporada">Fruta de Temporada</option>
+                            </select>
+                        </section>
+                        </div>
 
-                            <div>
-                            <section>
-                                <h2>Postre</h2>
-                                <select id="postre" required>
-                                <option value="" disabled selected>Seleccione un postre</option>
-                                <option value="Tarta de Queso">Tarta de Queso</option>
-                                <option value="Flan de Huevo">Flan de Huevo</option>
-                                <option value="Fruta de Temporada">Fruta de Temporada</option>
-                                </select>
-                            </section>
-                            </div>
 
-                        <p>Incluye: Pan y primera consumición.</p>
-                        <button type="submit">Enviar Menú</button>
+                <p>Incluye: Pan y primera consumición.</p>
+                 <button type="submit" id="enviar">Enviar Menú</button>
 
-                        </form>
-                 </main>
-            `
-        }) 
+                  </form>
 
+            `;
+// Volver a asociar el event listener al botón "Enviar" después de actualizar el HTML
+const buttonEnviar = document.querySelector("#enviar");
+buttonEnviar.addEventListener("click", (event) => handleClick(event));
+
+
+        }) ;
 
 }
 
-
 //-----------------------------------------------------------------------------------------------------//
-
 
 
 // ESCUCHADOR DE EVENTO. Al hacer click en Enviar ejecutamos función handleClick
 buttonEnviar.addEventListener("click", (event) => handleClick(event));
 
 
-//-----------------------------------------------------------------------------------------------------//
 
 
 
